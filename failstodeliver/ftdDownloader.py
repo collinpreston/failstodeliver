1a624bb581e4f6c76b632475ee078a176f82dbd7
curio-lighthouse <collin_preston@yahoo.com>
2021-12-19 10:06:15 -0500

    Only download one file

--- failstodeliver/ftdDownloader.py
+++ failstodeliver/ftdDownloader.py
@@ -5,9 +5,20 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import csv
+import psycopg2
+
+# this lambda should read from the a database table to get the filename
+# of the most recent, previously downloaded file.  Then it should
+# query the SEC page containing the list of files.  It will then
+# compare the filename of the first entry in the SEC list with the
+# filename of the most recently downloaded file in the database.
+# If the filenames are different, then it will download the new
+# SEC file and store that information within the database.
 
 
-
+conn = psycopg2.connect(
+   database="postgres", user='postgres', password='password', host='127.0.0.1', port= '5432'
+)
 
 def append_data(file_name, list_of_elem):
 
@@ -15,62 +26,33 @@ def append_data(file_name, list_of_elem):
         writer = csv.writer(write_obj)
         writer.writerows(list_of_elem)
 
-
-
-
 def download_csv():
         DOMAIN = "https://www.sec.gov"
         FAILS_TO_DELIVER_URL = "https://www.sec.gov/data/foiadocsfailsdatahtm"
         req = requests.get(FAILS_TO_DELIVER_URL)
         soup = BeautifulSoup(req.content, 'html.parser')
         a_list = soup.find_all('a', href=re.compile("^/files/data/"))
-        a_list_2 = soup.find_all('a', href=re.compile("^/files/node/add/"))
 
-        for a_item in a_list:
+        file_link = a_list[0].get('href', None)
+        file_url = DOMAIN + file_link
+        url = urllib.request.urlopen(file_url)
 
-            a_link = a_item.get('href', None)
-            file_url = DOMAIN + a_link
-            url = urllib.request.urlopen(file_url)
+        with ZipFile(BytesIO(url.read())) as my_zip_file:
+            for contained_file in my_zip_file.namelist():
+                temp_file_name = contained_file.split('.')
+                file_name = temp_file_name[0] + ".csv"
+                data = []
 
-            with ZipFile(BytesIO(url.read())) as my_zip_file:
-                for contained_file in my_zip_file.namelist():
-                    temp_file_name = contained_file.split('.')
-                    file_name = temp_file_name[0] + ".csv"
-                    print(file_name)
-                    data = []
+                for line in my_zip_file.open(contained_file).readlines():
+                    line = line.decode('ISO-8859-1')
+                    row = line.split('|')
+                    data.append(row)
 
-                    for line in my_zip_file.open(contained_file).readlines():
-                        line=line.decode('ISO-8859-1')
-                        row = line.split('|')
-                        data.append(row)
+                del data[-1]
+                del data[-1]
 
-                    del data[-1]
-                    del data[-1]
+                append_data(file_name, data)
 
-                    append_data(file_name,data)
-
-        for a_item in a_list_2:
-
-            a_link = a_item.get('href', None)
-            file_url = DOMAIN + a_link
-            url = urllib.request.urlopen(file_url)
-
-            with ZipFile(BytesIO(url.read())) as my_zip_file:
-                for contained_file in my_zip_file.namelist():
-                    temp_file_name = contained_file.split('.')
-                    file_name = temp_file_name[0] + ".csv"
-                    print(file_name)
-                    data = []
-
-                    for line in my_zip_file.open(contained_file).readlines():
-                        line=line.decode('ISO-8859-1')
-                        row = line.split('|')
-                        data.append(row)
-
-                    del data[-1]
-                    del data[-1]
-
-                    append_data(file_name,data)
 
 
 
